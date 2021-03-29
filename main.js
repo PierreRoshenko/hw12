ed083007605cec4b988a1e26da9ff46369f25a4f
PierreRoshenko <martik.assassin@gmail.com>
2021-03-29 15:50:30 +0300

    gg wp

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 async function getChinese(length) {
-    let res = [];
+    let res = '';
     for (let i = 0; i < length; i++) {
 
 
         const promise = new Promise((resolve) => {
             setTimeout(() => {
-                let date = 0;
-
-                date = Date.now().toString();
+                let date = Date.now().toString();
                 date = +date.split('').splice(-5).join('')
-
                 const chr = String.fromCharCode(date)
                 resolve(chr)
-
-
             }, 50);
 
         });
